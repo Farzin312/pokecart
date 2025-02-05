980d78483de396d5a5836e1d4154c715765df396
farzin312 <farzinshifat@gmail.com>
2025-02-04 20:19:32 -0500

    refactor: consolidate Firebase auth imports in firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
